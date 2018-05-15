f3ed430442d7a79ec8e6e11e2fa55e8d6b6c2124
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-05-15 11:29:20 +0200

    GDPR settings page final

--- jaz/script.js
+++ jaz/script.js
@@ -1,27 +1,38 @@
+var searchparams = window.location.search
+    .split('?')[1]
+    .split('&')
+    .map(function(val) {
+        return {
+            'name': val.split('=')[0],
+            'value': val.split('=')[1],
+        }
+    });
+
+var paramObject = {};
+searchparams.forEach(function(item) {
+    paramObject[item.name] = item.value;
+});
+
 $(document).ready(function() {
     if (document.location.href.indexOf('token') === -1) {
         $('.logged-out').css('display', 'block');
         $('.logged-in').css('display', 'none');
     }
 
-    var searchparams = window.location.search
-        .split('?')[1]
-        .split('&')
-        .map(function(val) {
-            return {
-                'name': val.split('=')[0],
-                'value': val.split('=')[1],
-            }
-        });
-    
-    var paramObject = {};
-    searchparams.forEach(function(item) {
-        paramObject[item.name] = item.value;
-    });
-
     $('#person').text(paramObject.email);
 
-    $('.switch input[type="checkbox"]').on('change', function() {
+    console.log('ping');
+
+    $.get('https://spam.djnd.si/get-settings/?token=' + paramObject.token + '&email=' + paramObject.email, function(r) {
+        console.log(r);
+        $('#checkbox-agrument')[0].checked = r.agrument;
+        $('#checkbox-djnd')[0].checked = r.djnd;
+        $('#checkbox-parlameter')[0].checked = r.parlameter;
+        $('#checkbox-konsenz')[0].checked = r.konsenz;
+        $('#konsenzname').val(r.name);
+    });
+
+    $('.switch input[type="checkbox"]').not('#checkbox-konsenz').on('change', function() {
         var target = $(this).parent().data('target');
         
         // call API here
@@ -33,6 +44,24 @@ $(document).ready(function() {
                 alert(r);
             }
         });
+    });
+
+    $('.switch-konsenz').on('click', function(event) {
+        if ($('#konsenzname').val() === '') {
+            alert('Da te lahko podpišemo, nam moraš povedati kako ti je ime.')
+            event.preventDefault();
+        } else {
+            var target = 'konsenz';
         
+            // call API here
+            url = 'https://spam.djnd.si/confirm-' + target + '/?token=' + paramObject.token + '&email=' + paramObject.email + '&name=' + $('#konsenzname').val() + '&permission=' + $(this).children('input')[0].checked;
+            console.log(url);
+            $.get(url, function(r) {
+                console.log(r);
+                if (r !== '1') {
+                    alert(r);
+                }
+            }); 
+        }
     });
 });
