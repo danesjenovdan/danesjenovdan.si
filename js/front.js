8d53368604195ec6b09411ebf8e93bfe53caea71
LepkoQQ <lepko.san@gmail.com>
2018-07-24 12:18:08 +0200

    add office phone number

--- js/front.js
+++ js/front.js
@@ -918,7 +918,7 @@ $(document).ready(function () {
     $('#izpostavljeno').data('href', r.url);
   });
 
-  $('.arrow-obcasnik-left, .arrow-obcasnik-right, #sendemail').not('.nextproject, .previousproject').on('click', function () {
+  $('.arrow-obcasnik-left, .arrow-obcasnik-right, #sendemail, #telefon').not('.nextproject, .previousproject').on('click', function () {
     window.location.href = $(this).data('href');
   });
 
