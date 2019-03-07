54228d39027c452a31d823ee2759c1c9428c308d
LepkoQQ <lepko.san@gmail.com>
2019-03-07 14:04:29 +0100

    fix eslint rules for inline nuxt-link

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,33 @@
+const INLINE_ELEMENTS = require('eslint-plugin-vue/lib/utils/inline-non-void-elements.json');
+
 module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:prettier/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
+  extends: ['@nuxtjs', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   // add your custom rules here
-  rules: {}
-}
+  rules: {
+    'vue/singleline-html-element-content-newline': [
+      'warn',
+      {
+        ignoreWhenNoAttributes: true,
+        ignoreWhenEmpty: true,
+        ignores: ['nuxt-link', 'pre', 'textarea', ...INLINE_ELEMENTS],
+      },
+    ],
+    'vue/multiline-html-element-content-newline': [
+      'warn',
+      {
+        ignoreWhenEmpty: true,
+        ignores: ['nuxt-link', 'pre', 'textarea', ...INLINE_ELEMENTS],
+        allowEmptyLines: false,
+      },
+    ],
+  },
+};
