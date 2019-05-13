146080a1afa0fdc5f6a6571109182fea9edb436a
LepkoQQ <lepko.san@gmail.com>
2019-05-13 16:29:14 +0200

    mini spacer fix

--- pages/about.vue
+++ pages/about.vue
@@ -45,7 +45,7 @@
         </div>
       </div>
     </div>
-    <div class="row my-5"/>
+    <div class="row my-3 py-xl-3"/>
     <div class="row box-links">
       <div class="col-12 col-sm-6 col-md-12 col-lg-6 col-xxl-4">
         <box-link/>
