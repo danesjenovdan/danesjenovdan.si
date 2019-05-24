b74a67946c82548038e0acafe431a29bcc14a5e5
LepkoQQ <lepko.san@gmail.com>
2019-05-24 12:01:52 +0200

    add vimeo embed url detection

--- components/PromotedTile.vue
+++ components/PromotedTile.vue
@@ -91,7 +91,12 @@ export default {
           return `https://www.youtube.com/embed/${m[1]}?rel=0&modestbranding=1`;
         }
       }
-      // TODO: vimeo
+      if (/[/.]vimeo\.com\//.test(v)) {
+        const m = /vimeo\.com\/(\d*?)(?:\?|$)/.exec(v);
+        if (m && m.length > 1) {
+          return `https://player.vimeo.com/video/${m[1]}`;
+        }
+      }
       return '';
     },
   },
