467b83607b2113cc1b2d2f3ea013971d72cfc384
LepkoQQ <lepko.san@gmail.com>
2019-05-28 14:07:41 +0200

    disable english site for now

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ module.exports = {
       {
         locales: [
           { code: 'sl', iso: 'sl-SI', file: 'sl-SI.json', name: 'Slovenščina' },
-          { code: 'en', iso: 'en-US', file: 'en-US.js', name: 'English' },
+          // { code: 'en', iso: 'en-US', file: 'en-US.js', name: 'English' },
         ],
         defaultLocale: 'sl',
         encodePaths: false,
