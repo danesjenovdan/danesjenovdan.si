49bd27c7afac449c7fcd112d6c25d674f3495e76
LepkoQQ <lepko.san@gmail.com>
2019-06-11 17:39:44 +0200

    product page beginning

--- pages/shop/_id.vue
+++ pages/shop/_id.vue
@@ -1,10 +1,38 @@
 <template>
-  <div>HELLO</div>
+  <div class="product">
+    <div class="product__container">
+      <h1 v-text="$te(`shop.products.${product.id}.display_name`) ? $t(`shop.products.${product.id}.display_name`) : product.name"/>
+      <div>{{ product }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  nuxtI18n: {
+    paths: {
+      sl: '/trgovina/:id/:slug?',
+      en: '/shop/:id/:slug?',
+    },
+  },
+  layout: 'checkout',
+  async asyncData({ $axios, params }) {
+    const products = await $axios.$get('https://podpri.djnd.si/api/shop/products/');
+    return {
+      product: products.find(p => p.id === Number(params.id)),
+      params,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.product {
+  h1 {
+    font-size: 1.85rem;
+    text-align: center;
+    font-weight: 600;
+    margin: 3rem 0;
+  }
+}
 </style>
