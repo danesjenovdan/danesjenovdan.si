973150b90ee400d1200f223e50df16e66569c66d
LepkoQQ <lepko.san@gmail.com>
2019-07-11 18:22:42 +0200

    finish product page with working buy now button

--- pages/shop/_id.vue
+++ pages/shop/_id.vue
@@ -3,13 +3,37 @@
     <div class="product__container">
       <h1 v-text="getDisplayName(product)" />
       <h2 class="price">{{ formatPrice(product.price) }}</h2>
-      <div>{{ product }}</div>
+      <template v-if="$te(`shop.products.${product.id}.images`)">
+        <img v-for="img in $t(`shop.products.${product.id}.images`)" :key="img" :src="img" />
+      </template>
+      <template v-else>
+        <img :src="`/img/products/${product.id}.jpg`" />
+      </template>
+      <p v-t="`shop.products.${product.id}.description`" />
+      <hr />
+      <div>TODO: variants</div>
+      <hr />
+      <more-button
+        block
+        color="secondary"
+        icon="heart"
+        :to="'#'"
+        :text="'KUPI'"
+        @click.native.prevent="addAndCheckout"
+      />
+      <div class="add-to-basket">
+        <nuxt-link to="#">
+          Dodaj v voziček in še malo pobrskaj po
+          policah
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import productMixin from '~/mixins/product.js';
+import MoreButton from '~/components/MoreButton.vue';
+import shopMixin from '~/mixins/shop.js';
 
 export default {
   nuxtI18n: {
@@ -19,13 +43,32 @@ export default {
     },
   },
   layout: 'checkout',
-  mixins: [productMixin],
+  components: {
+    MoreButton,
+  },
+  mixins: [shopMixin],
+  data() {
+    return {
+      orderKey: null,
+    };
+  },
   async asyncData({ $axios, params }) {
     const products = await $axios.$get('https://podpri.djnd.si/api/shop/products/');
     return {
       product: products.find(p => p.id === Number(params.id)),
       params,
     };
+  },
+  async mounted() {
+    if (typeof window !== 'undefined') {
+      this.orderKey = await this.getOrderKey();
+    }
+  },
+  methods: {
+    async addAndCheckout() {
+      await this.addToBasket(this.orderKey, this.product.id, 1);
+      this.$router.push(this.localePath('shop-checkout'));
+    },
   },
 };
 </script>
@@ -45,6 +88,40 @@ export default {
     font-size: 1.85rem;
     font-weight: 300;
     font-style: italic;
+  }
+
+  img {
+    width: 100%;
+    max-width: 100%;
+    margin-bottom: 2rem;
+  }
+
+  p {
+    font-size: 1.2rem;
+    font-weight: 300;
+    font-style: italic;
+  }
+
+  hr {
+    border-top-color: #686d6e;
+    margin-top: 1.75rem;
+    margin-bottom: 1.75rem;
+  }
+
+  .add-to-basket {
+    margin: 2rem 0 1rem;
+    color: #333;
+
+    a {
+      text-decoration: underline;
+      color: inherit;
+      font-style: italic;
+      font-weight: 300;
+
+      &:hover {
+        text-decoration: none;
+      }
+    }
   }
 }
 </style>
