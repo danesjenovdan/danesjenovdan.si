0067439551f4dff69354870caccb5dc9131bdee8
LepkoQQ <lepko.san@gmail.com>
2019-11-08 15:52:39 +0100

    add simple error page, so that the default nuxt error page doesnt break the layout

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <page-title :title="'Error'" :text="error" />
+  </div>
+</template>
+
+<script>
+import PageTitle from '~/components/PageTitle.vue';
+
+//                                                                            //
+//             WARNING: this is treated as a page, not a layout!              //
+//                                                                            //
+
+export default {
+  components: {
+    PageTitle,
+  },
+  props: {
+    error: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
