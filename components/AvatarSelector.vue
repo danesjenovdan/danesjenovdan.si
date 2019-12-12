99bc4e3c412eeefd3b57ca1ec981bb76afe42022
LepkoQQ <lepko.san@gmail.com>
2019-12-12 12:11:15 +0100

    improve avatar selector and auto append custom user images to image strip and select and position images correctly on the strip

--- components/AvatarSelector.vue
+++ components/AvatarSelector.vue
@@ -9,9 +9,10 @@
           class="images"
         >
           <button
-            v-for="i in 5"
-            :key="i"
+            v-for="(avatar, i) in avatars"
+            :key="avatar.image"
             :class="['avatar-image', { selected: i === selectedIndex }]"
+            :style="{ backgroundImage: `url('${avatar.image}')` }"
             @click="selectedIndex = i"
             type="button"
           ></button>
@@ -26,10 +27,18 @@
         >
       </div>
       <client-only placeholder="Loading...">
-        <img :src="userAvatar" alt="" />
         <avatar-cropper
           :labels="{ submit: 'Izberi', cancel: 'Prekliči' }"
           :upload-handler="handleUpload"
+          :cropper-options="{
+            background: false,
+            aspectRatio: 1,
+            autoCropArea: 1,
+            viewMode: 0,
+            movable: false,
+            zoomable: false,
+            dragMode: 'move',
+          }"
           trigger="#customFile"
         />
       </client-only>
@@ -40,33 +49,57 @@
 <script>
 export default {
   data() {
+    const avatars = [
+      {
+        image: '/avatars/avatar_dinosaur.svg',
+      },
+      {
+        image: '/avatars/avatar_ninja.svg',
+      },
+      {
+        image: '/avatars/avatar_astronaut.svg',
+      },
+      {
+        image: '/avatars/avatar_icecream.svg',
+      },
+      {
+        image: '/avatars/avatar_ghost.svg',
+      },
+    ];
     return {
-      selectedIndex: 3,
-      userAvatar: undefined,
-      showCropper: true,
-      imagesOffset: 0,
+      selectedIndex: Math.floor(avatars.length / 2),
+      avatars,
     };
   },
-  watch: {
-    selectedIndex(newValue) {
+  computed: {
+    imagesOffset() {
       // average of (big image + margin) and (small image + margin) + 8 (no idea why)
       const avg = (80 + 12 + 64 + 12 + 8) / 2;
-      const centerIndex = newValue - 3;
-      this.imagesOffset = centerIndex * avg * -1;
-      console.log(this.imagesOffset);
+      // this shifts 0...x indexes to -x/2...x/2 with 0 in the middle
+      let shifted = this.selectedIndex - Math.floor(this.avatars.length / 2);
+      if (this.avatars.length % 2 === 0) {
+        // even number of avatars means center is between 2 avatars, so move over by half
+        shifted += 0.5;
+      }
+      return shifted * avg * -1;
     },
   },
   methods: {
     handleUpload(cropper) {
       // TODO: use to blob and toblob polyfill
-      this.userAvatar = cropper
+      const userAvatar = cropper
         .getCroppedCanvas({
-          width: 200,
-          height: 200,
+          width: 400,
+          height: 400,
           maxWidth: 2048,
           maxHeight: 2048,
+          fillColor: '#fff',
         })
-        .toDataURL('image/jpeg', 0.6);
+        .toDataURL('image/jpeg', 0.7);
+      this.avatars.push({
+        image: userAvatar,
+      });
+      this.selectedIndex = this.avatars.length - 1;
     },
   },
 };
@@ -114,14 +147,15 @@ export default {
         height: 100%;
         align-items: center;
         justify-content: center;
+        transition: transform 0.25s ease;
 
         .avatar-image {
           width: 4rem;
           height: 4rem;
           border: none;
           background-color: #fff;
-          background-size: 80% 80%;
-          background-position: center bottom;
+          background-size: 100% 100%;
+          background-position: center center;
           background-repeat: no-repeat;
           margin: 0 0.75rem;
           padding: 0;
@@ -135,22 +169,6 @@ export default {
             height: 5rem;
             border: 2px solid $color-red;
           }
-        }
-
-        .avatar-image:nth-of-type(1) {
-          background-image: url('/avatars/avatar_dinosaur.svg');
-        }
-        .avatar-image:nth-of-type(2) {
-          background-image: url('/avatars/avatar_ninja.svg');
-        }
-        .avatar-image:nth-of-type(3) {
-          background-image: url('/avatars/avatar_astronaut.svg');
-        }
-        .avatar-image:nth-of-type(4) {
-          background-image: url('/avatars/avatar_icecream.svg');
-        }
-        .avatar-image:nth-of-type(5) {
-          background-image: url('/avatars/avatar_ghost.svg');
         }
       }
     }
@@ -176,6 +194,32 @@ export default {
           display: none;
         }
       }
+    }
+  }
+
+  .avatar-cropper /deep/ {
+    .avatar-cropper-mark {
+      background-color: rgba(#000, 0.5);
+
+      .avatar-cropper-close {
+        color: #000;
+        font-weight: 700;
+        line-height: 1;
+        width: 1em;
+        height: 1em;
+        padding: 0;
+      }
+    }
+
+    .cropper-view-box {
+      outline: 1px solid $color-red;
+      outline-color: rgba($color-red, 0.75);
+      background-color: #fff;
+    }
+
+    .cropper-line,
+    .cropper-point {
+      background-color: $color-red;
     }
   }
 }
