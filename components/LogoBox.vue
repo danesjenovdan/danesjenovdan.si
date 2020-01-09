3efce5c3a66d7e27671c78dcf0d1a8e9d5d6a715
LepkoQQ <lepko.san@gmail.com>
2020-01-09 17:42:02 +0100

    move partners and donators to json file, fix some logos, add hover state

--- components/LogoBox.vue
+++ components/LogoBox.vue
@@ -6,7 +6,18 @@
       rel="noopener noreferrer"
       class="logo-box__content"
     >
-      <img :src="image" :alt="`${name} logo`" />
+      <div class="image-container" :class="[contentClass]">
+        <img :src="image" :alt="`${name} logo`" />
+      </div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="30 18 40 64"
+        fill="currentColor"
+      >
+        <path
+          d="M50.005 82c.715-.028 1.542-.322 2.063-.812l17-16c.975-1.085 1.377-3.164.25-4.375-1.109-1.194-3.26-1.159-4.375.03l-11.938 11.25V21a3 3 0 0 0-6 0v51.094l-11.938-11.25c-1.025-1.024-3.253-1.213-4.375-.031-1.122 1.181-.764 3.335.25 4.375l17 16a2.885 2.885 0 0 0 2.063.812z"
+        />
+      </svg>
     </a>
   </div>
 </template>
@@ -26,6 +37,10 @@ export default {
       type: String,
       required: true,
     },
+    contentClass: {
+      type: String,
+      default: undefined,
+    },
   },
 };
 </script>
@@ -40,20 +55,65 @@ export default {
   background-color: #fff;
 
   .logo-box__content {
-    display: flex;
-    align-items: center;
-    justify-content: center;
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    padding: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-    img {
+    &:hover {
+      &::before {
+        opacity: 0.8;
+      }
+
+      svg {
+        opacity: 1;
+      }
+    }
+
+    .image-container {
+      position: absolute;
+      top: 0;
+      left: 0;
       width: 100%;
-      mix-blend-mode: darken;
-      filter: grayscale(100%);
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 2rem;
+
+      img {
+        width: 100%;
+        filter: grayscale(100%);
+      }
+    }
+
+    svg {
+      width: 3rem;
+      height: 3rem;
+      position: relative;
+      color: #fff;
+      transform: rotate(-90deg);
+    }
+
+    &::before {
+      content: '';
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: $color-green;
+    }
+
+    &::before,
+    svg {
+      opacity: 0;
+      transition: opacity 0.15s ease;
+      z-index: 2;
     }
   }
 }
