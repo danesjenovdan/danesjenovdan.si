509c24e922b1e8f742399f4c68b60cf79dfd2e48
LepkoQQ <lepko.san@gmail.com>
2020-01-10 12:32:25 +0100

    fix contrast on shop bar between text and bg image, add some white bg under text

--- components/ShopBar.vue
+++ components/ShopBar.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="shop-bar bg-white">
     <div class="bar-container">
-      <strong v-t="'shop.promo-bar.support-us'" class="my-xl-3" />
+      <div class="bar-underlay py-1 px-2">
+        <strong v-t="'shop.promo-bar.support-us'" class="my-xl-3" />
+      </div>
       <img
         src="/img/shop-banner.jpg"
         alt="shirts from our shop"
         class="img-fluid d-block d-xl-none"
       />
-      <more-button
-        :to="localePath('shop')"
-        :text="$t('shop.promo-bar.button-text')"
-        color="secondary"
-        small
-      />
+      <div class="bar-underlay">
+        <more-button
+          :to="localePath('shop')"
+          :text="$t('shop.promo-bar.button-text')"
+          color="secondary"
+          small
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -53,11 +57,9 @@ export default {
     strong {
       text-align: center;
       max-width: 270px;
-      margin-bottom: 1rem;
 
       @include media-breakpoint-up(xl) {
         max-width: initial;
-        margin-bottom: 0;
       }
     }
 
@@ -65,6 +67,15 @@ export default {
       width: calc(100% + #{$content-mobile-padding * 2});
       max-width: calc(100% + #{$content-mobile-padding * 2});
       margin: 0 #{-$content-mobile-padding};
+    }
+
+    .bar-underlay {
+      background-color: rgba(#fff, 0.8);
+      margin: 1rem 0;
+
+      @include media-breakpoint-up(xl) {
+        margin: 0;
+      }
     }
   }
 }
