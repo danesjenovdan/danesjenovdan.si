809db5d89f710f258ab0995f0a428b5e02d0f63b
muki <filipdobranic@gmail.com>
2020-11-26 17:34:56 +0100

    remove personal pickup from shop and style UPN

--- components/Payment/Upn.vue
+++ components/Payment/Upn.vue
@@ -2,8 +2,9 @@
   <div class="upn-payment">
     <div v-if="error" class="alert alert-danger">{{ error }}</div>
     <form v-else>
+      <img src="/img/poloznica.png" id="poloznica-img" />
       <div class="form-group">
-        <label>Po e-pošti ti bomo poslali položnico</label>
+        <label>Položnico za plačilo prejmeš na svoj e-naslov.</label>
       </div>
       <more-button
         :to="localePath('shop-checkout')"
@@ -51,6 +52,17 @@ export default {
     font-weight: 300;
     text-align: center;
     display: block;
+    padding-top: 10px;
+    padding-bottom: 30px;
+  }
+
+  #poloznica-img {
+    width: 100px;
+    height: auto;
+    margin: auto;
+    display: block;
+    margin-bottom: 10px;
+    padding-top: 40px;
   }
 }
 </style>
