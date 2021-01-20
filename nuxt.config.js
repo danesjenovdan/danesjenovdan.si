c22a76ac1106879b42189a1504ec11815a3a831c
LepkoQQ <lepko.san@gmail.com>
2021-01-20 11:54:21 +0100

    fix default title

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,7 @@ export default {
    ** Headers of the page
    */
   head: {
+    title: '',
     titleTemplate: (titleChunk) => {
       return titleChunk
         ? `${titleChunk} - Danes je nov dan`
