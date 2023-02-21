d7d284e0b59eae3478fc2ffc8224ecfffa006d72
patricija <patricija@danesjenovdan.si>
2023-02-21 14:21:51 +0100

    citizens assemblies invitation

---
+++ pages/articles/vabilo-citizens-assemblies.vue
@@ -0,0 +1,360 @@
+<template>
+  <div>
+    <news-article
+      :title="title"
+      :date="date"
+      :logo-src="'/img/articles/zbori-obcank-hero.jpg'"
+      :logo-alt="''"
+      class="custom"
+    >
+      <p>
+        <strong
+          >Zbori občank_ov so mednarodno vse bolj razširjen in priznan način
+          vključevanja javnosti v delo lokalne oblasti. Danes je nov dan v
+          sodelovanju s poljsko organizacijo
+          <i>Center for Blue Democracy</i> vabi na usposabljanje za organizacijo
+          in vodenje zborov občank_ov.</strong
+        >
+      </p>
+      <br />
+      <h4>
+        <strong>Kdaj in kje?</strong>
+      </h4>
+      <p>
+        <strong>Datum in čas:</strong> usposabljanje se bo začelo v sredo, 10.
+        maja 2023, ob 10.00 in bo trajalo do petka, 12. maja 2023, do 16.30.
+      </p>
+      <p>
+        <strong>Lokacija:</strong> dogodek bomo izvedli v Hotelu Kompas v
+        Kranjski Gori.
+      </p>
+      <h4>
+        <strong>O zborih občank_ov</strong>
+      </h4>
+      <p>
+        Zbore občank_ov slovenski pravni red razume kot način posvetovanja
+        občine s svojimi prebivalci_kami. Na žalost zbori občank_ov, ki jih
+        organizirajo občine, prevečkrat temeljijo na enostranskih predstavitvah
+        in ne dialogu, proces organiziranja dogodka ni transparenten ter
+        vključujoč, zaključki posveta pa niso zavezujoči za odločevalce_ke.
+      </p>
+      <p>
+        Medtem zbori občank_ov oz. citizens’ assemblies v mednarodnem okolju v
+        zadnjih 15 letih postajajo vedno bolj priljubljeni, kot inovativno
+        metodo vključevanja prebivalk_cev v delovanje njihove skupnosti pa jih
+        med drugim prepoznavata OECD in Svet Evrope. Modela se poslužuje tudi EU
+        – ta je pred kratkim zaključila Konferenco o prihodnosti Evrope, ki je
+        temeljila na različnih odprtih in vključujočih demokratičnih zborih
+        prebivalk_cev.
+      </p>
+      <p>
+        Zbori občank_ov so lahko vzvod bolj demokratičnega in vključujočega
+        delovanja občin, z vključevanjem tradicionalno nezainteresiranih in
+        izključenih skupin prebivalk_cev pa lahko vodijo do boljših politik. Gre
+        za metodo, ki išče soglasje in konsenz, s čimer je lahko protiutež
+        nezaupanju in političnim delitvam, ki prežemajo družbeni prostor.
+      </p>
+      <p>
+        Zbore prebivalk_cev je organiziralo že več evropskih držav, redno pa jih
+        organizirajo tudi mnoge regije in občine. Čas je, da zbori občank_ov
+        postanejo orodje demokracije tudi pri nas!
+      </p>
+      <h4>
+        <strong>O usposabljanju</strong>
+      </h4>
+      <p>
+        Usposabljanje je namenjeno vsem, ki se ukvarjate z demokracijo in
+        participacijo ter vas zbori občank_ov zanimajo. Namen tridnevnega
+        dogodka je usposobiti sedanje in prihodnje (so)organizatorke_je zborov
+        občank_ov. Predavatelji iz organizacije Center for Blue Democracy so z
+        dolgoletnim podpiranjem in vodenjem deliberativnih procesov nabrali
+        bogate izkušnje, ki so jih pripeljale do razvoja lastne metodologije, to
+        pa zdaj predstavljajo širši skupnosti tistih, ki želijo tudi sami
+        organizirati zbore občank_ov. Njihov cilj je, da so vsi tovrstni zbori
+        dobro zasnovani, da upoštevajo visoke standarde kakovosti ter da se
+        izvajajo z zaupanjem, lahkoto in veseljem. Glavni del usposabljanja je
+        torej namenjen krepitvi znanj in zmožnosti za uresničitev tega cilja.
+      </p>
+      <h4>
+        <strong>Komu je usposabljanje namenjeno?</strong>
+      </h4>
+      <p>
+        Usposabljanja se lahko udeležijo vsi_e člani_ce organizacij in skupin,
+        ki bi si želeli izvesti in voditi zbore občank_ov – tako tistim, ki že
+        imajo izkušnje na tem področju, kot tudi tistim, ki se s tovrstnimi
+        aktivnostmi še niso srečali. Na usposabljanje pa se lahko prijavite tudi
+        neodvisni_e posamezniki_ce, ki se zavzemate za demokratične inovacije in
+        sistemske reforme za večjo vključenost občank_ov v demokratične procese.
+      </p>
+      <h4>
+        <strong>Zakaj se pridružiti?</strong>
+      </h4>
+      <p>
+        Na delavnici boste lahko spoznali postopek organizacije zborov občank_ov
+        v skladu s tako imenovanim oblikovalskim razmišljanjem (design
+        thinking). Za nekatere bo to povsem nova izkušnja, drugi_e pa lahko
+        nadgradite svoje znanje in razširite obstoječe izkušnje. To je tudi
+        priložnost, da spoznate druge organizatorje_ke zborov občank_ov oziroma
+        tiste, ki želijo to še postati, s tem pa dobite dodaten navdih za lastno
+        delo. Poleg praktičnih vidikov organizacije zborov ter vrednot in načel,
+        ki ta proces navdihujejo, se bomo na usposabljanju osredotočili tudi
+        osebno rast in izboljšanje mehkih veščin. Dogodek bo potekal v
+        sproščenem, prijateljskem in neformalnem vzdušju, v katerem bo učenje
+        podprto z razpravami, vizualnimi prikazi, skupinskimi vajami, miselnimi
+        eksperimenti in podobnimi orodji.
+      </p>
+      <h4>
+        <strong>Format dogodka</strong>
+      </h4>
+      <p>
+        Usposabljanje bo potekalo tri dni, v tem času pa se bomo poglobili v
+        vprašanja kaj, zakaj in kako se lotiti organizacije ter koordinacije
+        zborov občank_ov. Udeležence_ke bomo v tem času opremili z znanjem,
+        veščinami in odnosi, ki so v skladu z našo metodologijo potrebni za
+        uspešno vodenje tovrstnih zborov. Med drugim bomo obravnavali naslednje
+        teme:
+      </p>
+      <ol>
+        <li>
+          Demokratično razmišljanje – kakšen pomen imajo posvetovanja in
+          državljanska opolnomočenost, kakovost in količina v demokratičnih
+          procesih ter podpora odločevalk_cev? Zanimalo nas bo tudi razmerje med
+          demokracijo in dobrim upravljanjem, spraševali pa se bomo še o tem,
+          kaj sploh je demokracija.
+        </li>
+        <li>
+          Standardi in vodila pri organizaciji zborov – kakšni naj bodo
+          standardi in načela pri organizaciji ter vodenju zborov občank_ov,
+          kako jih vključiti na različnih stopnjah tega procesa in kakšno vlogo
+          imajo z vidika verodostojnosti?
+        </li>
+        <li>
+          Struktura upravljanja zborov – kakšne so naloge in odnosi med skupino
+          za oblikovanje, skupino za usklajevanje, skupino za spodbujanje in
+          skupino za monitoring zborov občank_ov?
+        </li>
+        <li>
+          Vloge v zborih občank_ov – kakšne so pravice in odgovornosti ter kako
+          pridobiti člane_ice skupščine, opazovalce_ke, strokovnjake_inje, priče
+          ter druge deležnike_ce?
+        </li>
+        <li>
+          Pregled nad procesom organizacije zborov – kakšne so faze in časovni
+          okvir za: pošiljanje vabil, naključen izbor članov_ic in njihovo
+          usposabljanje ter učno, deliberativno in fazo pregleda? Kako
+          oblikovati pravilnik?
+        </li>
+        <li>
+          Izbira tem zborov – kakšno je razmerje med temo zbora in časovnico,
+          kako določiti kriterije za izbor teme, kako izvesti postopek izbire?
+        </li>
+        <li>
+          Ustanovitev zbora občank_ov – koliko članic_ov naj ima zbor, kako jih
+          povabiti, katere so različne metode naključnega izbora, kako
+          zagotoviti transparentnost postopka in kakšna so merila
+          stratifikacije?
+        </li>
+        <li>
+          Faza usposabljanje članic_ov – kako naj poteka priprava članic_ov na
+          postopek izbire, kako oblikovati poskusni zbor kot uvodno vajo?
+        </li>
+        <li>
+          Učna faza – kako identificirati ustrezne strokovnjake_inje in kako
+          komunicirati z njimi, kako iskati prave deležnike_ce in kako
+          komunicirati z njimi ter kako oblikovati učni program?
+        </li>
+        <li>
+          Deliberativna faza – kakšna so načela posvetovanja, kako pridobiti
+          osnovne informacije o skupinskih procesih? Kako zagotoviti najboljše
+          delo skupin – opolnomočenje, odgovornost in znanje? Kako potekajo
+          pregled, razprava in spreminjanje predlaganih priporočil ter kako
+          sprejeti odločitve in predstaviti rezultate?
+        </li>
+        <li>
+          Promocija in komunikacija – kako promovirati zbore občank_ov v
+          tradicionalnih in družbenih medijih? Kakšen jezik in katere načine
+          komuniciranja uporabljati?
+        </li>
+        <li>
+          Nadaljnje ukrepanje – kako implementirati rezultate in kako izvajati
+          nadzor nad postopkom implementacije?
+        </li>
+      </ol>
+      <h4>
+        <strong>Več informacij o temi in usposabljanju</strong>
+      </h4>
+      <p>
+        Za vse, ki vas zanima udeležba na usposabljanju, a niste povsem
+        prepričani, da je ta dogodek namenjen vam, oziroma želite izvedeti več o
+        programu, bomo izvedli predhodno predstavitev. Ta bo potekala v
+        <strong>sredo, 8. marca, ob 11. uri</strong> prek spleta.
+      </p>
+      <p>
+        Za prijavo na predstavitev usposabljanja nam pišite na e-naslov
+        <a href="mailto:mateo@danesjenovdan.si">mateo@danesjenovdan.si</a>, in
+        sicer najkasneje do ponedeljka, 6. marca.
+      </p>
+      <h4>
+        <strong>Usposabljanje bodo vodili</strong>
+      </h4>
+      <p>
+        <strong>Dr. Marcin Gerwin</strong> – je strokovnjak za deliberativno
+        demokracijo in trajnostni razvoj. Koordinira organizacijo Center for
+        Blue Democracy, pobudo za deliberativno demokracijo z globalnim dosegom.
+        Diplomiral je iz politologije, tema njegove doktorske disertacije pa je
+        bila trajnostni razvoj v kontekstu globalnih izzivov. Oblikuje
+        državljanske skupščine in nove oblike demokracije. Je avtor knjig
+        <i>“Citizens’ Assemblies: Guide to democracy that works”</i>,
+        <i>“Deliberative Café: An Easy to Organise Citizens' Asssembly”</i>,
+        <i>”Deliberative Democracy: Waldenia Model”</i>,
+        <i>”Citizens' Chamber: A Blueprint for New Democracy”</i>, in
+        <i>”European Citizens' Assembly: A New Model for Decision-Making”</i>.
+      </p>
+      <p>
+        <strong>Zuzanna Nowak</strong> – diplomirala je iz mednarodnih odnosov s
+        specializacijo iz diplomacije in konzularnih odnosov. Je usposobljena
+        moderatorka z več kot sedmimi leti izkušenj pri oblikovanju in vodenju
+        izobraževalnih ter deliberativnih procesov z uporabo neformalne
+        metodologije. V različnih vlogah, kot strokovna svetovalka,
+        soustvarjalka, moderatorka in drugo, podpira zbore državljank_ov na
+        Poljskem in zunaj nje. Njeno največje poklicno veselje je pomagati
+        ljudem, da se počutijo opolnomočene in da uresničijo svoje cilje (ter da
+        izdeluje lepe plakate). V zasebnem življenju je umetnica, študentka joge
+        in strastna kuharica.
+      </p>
+      <p>
+        <strong>Eva Mackeviča</strong> – je strokovnjakinja z več kot petimi
+        leti izkušenj na področju vodenja projektov, specializirana za
+        izobraževalne in participativne projekte. Koordinirala in vodila je
+        zborovanje državljanov v Poznanu, s Svetom Evrope pa sodeluje kot
+        oblikovalka procesov v projektu "Krepitev participativne demokracije in
+        človekovih pravic" v Gruziji. Zanimajo jo reševanje konfliktov,
+        državljansko udejstvovanje in pravni pristop k sistemskim spremembam.
+      </p>
+      <h4>
+        <strong>Jezik</strong>
+      </h4>
+      <p>Celotno usposabljanje bo potekalo v angleščini.</p>
+      <h4>
+        <strong>Program</strong>
+      </h4>
+      <p>Sreda, 10. maja, od 10.00 do 16.30 (z eno uro odmora za kosilo)</p>
+      <p>Četrtek, 11. maja, od 10.00 do 16.30 (z eno uro odmora za kosilo)</p>
+      <p>Petek, 12. maja, od 10.00 do 16.30 (z eno uro odmora za kosilo)</p>
+      <h4>
+        <strong>Cena</strong>
+      </h4>
+      <p>
+        Udeležba na usposabljanju je brezplačna, udeleženci_ke pa si morajo sami
+        kriti potne stroške in stroške nastanitve.
+      </p>
+      <h4>
+        <strong>Nastanitev</strong>
+      </h4>
+      <p>
+        Za vse udeležence smo pripravili posebno ponudbo za nastanitev v Hotelu
+        Kompas, ki so jo lahko ogledate
+        <a
+          href="https://secure.phobs.net/book.php?page=availability&companyid=421&hotelid=2189&checkin=2023-05-10&checkout=2023-05-12&partnerid=15671&crcid=988c03b9601ce669d6b2fa9edd1c1e3a"
+          target="_blank"
+          >TUKAJ</a
+        >.
+      </p>
+      <h4>
+        <strong>Registracija</strong>
+      </h4>
+      <p>
+        Za registracijo obiščite spodnjo povezavo:
+        <a
+          href="https://www.eventbrite.ie/e/training-camp-for-coordinators-of-citizens-assemblies-kranjska-gora-tickets-551288778577"
+          target="_blank"
+          >https://www.eventbrite.ie/e/training-camp-for-coordinators-of-citizens-assemblies-kranjska-gora-tickets-551288778577</a
+        >.
+      </p>
+      <p>Registracija je odprta do zapolnitve kapacitet.</p>
+      <h4>
+        <strong>Dodatne informacije</strong>
+      </h4>
+      <p>
+        Za vsa vprašanja nas kontaktirajte na e-naslov
+        <a href="mailto:mateo@danesjenovdan.si">mateo@danesjenovdan.si</a>.
+      </p>
+    </news-article>
+  </div>
+</template>
+
+<script>
+import NewsArticle from '~/components/NewsArticle.vue';
+
+export default {
+  pageColor: 'primary',
+  nuxtI18n: {
+    paths: {
+      sl: '/zbori-obcanov',
+      en: '/citizens-assemblies',
+    },
+  },
+  components: {
+    NewsArticle,
+  },
+  data() {
+    return {
+      title: 'Usposabljanje: zbori občank_ov',
+      date: '2023-02-21',
+    };
+  },
+  head() {
+    return {
+      title: 'Pridružite se usposabljanju na temo zborov občank_ov!',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Zbori občank_ov so vse bolj priznan način vključevanja javnosti v delo lokalne oblasti. Udeležite se usposabljanja za njihovo organizacijo in vodenje.',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Pridružite se usposabljanju na temo zborov občank_ov!',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Zbori občank_ov so vse bolj priznan način vključevanja javnosti v delo lokalne oblasti. Udeležite se usposabljanja za njihovo organizacijo in vodenje.',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://danesjenovdan.si/og-zbori-obcank.jpg',
+        },
+        {
+          hid: 'twitter:title',
+          property: 'twitter:title',
+          content: 'Pridružite se usposabljanju na temo zborov občank_ov!',
+        },
+        {
+          hid: 'twitter:description',
+          property: 'twitter:description',
+          content:
+            'Zbori občank_ov so vse bolj priznan način vključevanja javnosti v delo lokalne oblasti. Udeležite se usposabljanja za njihovo organizacijo in vodenje.',
+        },
+        {
+          hid: 'twitter:image',
+          property: 'twitter:image',
+          content: 'https://danesjenovdan.si/og-zbori-obcank.jpg',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.custom {
+  /deep/ .article-top-logo img {
+    max-width: 100% !important;
+  }
+}
+</style>
