9c8d27eeace813457a0f1c2d09f4b25a9e609944
Patricija Brečko <patricija@danesjenovdan.si>
2023-09-11 14:34:00 +0200

    hack velenje page

---
+++ pages/hackathons/hack-velenje-2023.vue
@@ -0,0 +1,667 @@
+<template>
+  <main class="overflow-hidden">
+    <section id="section-hero" class="container-fluid">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-xxl-10 offset-xxl-2">
+            <h1>HACK VELENJE</h1>
+            <h2>Hekaton z odprtimi občinskimi podatki</h2>
+            <h3>22. september @ SAŠA inkubator, Velenje</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6 offset-xxl-2">
+            <p>
+              Pridi na enodnevni hekaton v SAŠA inkubator, spoznaj odprte zbirke
+              podatkov Mestne občine Velenje in razvij koncept, ki bo izboljšal
+              vsakdanje življenje občanov in občank.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section
+      id="section-graphics"
+      class="container-fluid d-none d-md-flex justify-content-between align-items-center"
+    >
+      <img src="/icons/hackathons/emojis.png" />
+      <div class="graphics-divider"></div>
+      <img src="/icons/hackathons/dj.png" />
+      <div class="graphics-divider"></div>
+      <img src="/icons/hackathons/tablet.png" />
+      <div class="graphics-divider"></div>
+      <img src="/icons/hackathons/unicorn.png" />
+      <div class="graphics-divider"></div>
+      <img src="/icons/hackathons/reaction.png" />
+    </section>
+
+    <section id="section-invite" class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-lg-8">
+            <p>
+              Imaš idejo, kako zasnovati interaktivni seznam javnih košarkarskih
+              igrišč v Velenju? Ali pa predstavitev najlepših sprehajalnih poti
+              v okolici? Želiš razviti mobilno aplikacijo, ki izboljša
+              medsosedsko komunikacijo? V poštev pridejo vsi koncepti in
+              prototipi, predvsem pa izvirne domislice vseh vrst in oblik… Kdo
+              ve, morda se prav tvoja ideja znajde na stopničkah, ti pa se domov
+              odpraviš z denarno nagrado!
+            </p>
+            <p>
+              Pridruži se nam v ekipi, lahko pa prideš tudi sam_a in si
+              somišljenike_ce najdeš na dogodku! Zahtevamo le, da si
+              polnoleten_a in da gojiš ljubezen do informacijske tehnologije.
+              Vabljeni vsi programerji_ke, oblikovalci_ke in ostali tehnološki
+              navdušenci!
+            </p>
+            <p><strong>Se vidimo?</strong></p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col d-flex justify-content-center mt-4 mt-md-0">
+            <a
+              type="button"
+              href="https://forms.gle/U1JNKgxHmb2XqR8Q6"
+              target="_blank"
+            >
+              PRIJAVI SE!
+            </a>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p class="text-center registration-open mt-3 mb-0">
+              Prijave zbiramo do 22. septembra.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-awards" class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-lg-8">
+            <h2>NAGRADNI SKLAD</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center my-4">
+          <div class="col-12 col-lg-8">
+            <div class="row">
+              <div class="col-md-6 d-flex align-items-end first-prize">
+                <h3 class="mr-4 mb-0">1. mesto:</h3>
+                <h2 class="mb-0">700€</h2>
+              </div>
+              <div class="col-md-6 d-flex align-items-end second-prize">
+                <h3 class="mr-4 mb-0">2. in 3. mesto:</h3>
+                <h2 class="mb-0">150€</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-agenda" class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-lg-8">
+            <h2>AGENDA</h2>
+            <div class="row">
+              <div class="col-3 col-md-2">9:00</div>
+              <div class="col-9 col-md-10">Registracija</div>
+            </div>
+            <div class="row">
+              <div class="col-3 col-md-2">9:30</div>
+              <div class="col-9 col-md-10">Uvodni nagovor</div>
+            </div>
+            <div class="row">
+              <div class="col-3 col-md-2">10:00</div>
+              <div class="col-9 col-md-10">Delo na projektih</div>
+            </div>
+            <div class="row">
+              <div class="col-3 col-md-2">15:00</div>
+              <div class="col-9 col-md-10">Predstavitve rešitev</div>
+            </div>
+            <div class="row">
+              <div class="col-3 col-md-2">16:30</div>
+              <div class="col-9 col-md-10">Odmor, komisija se sestane</div>
+            </div>
+            <div class="row">
+              <div class="col-3 col-md-2">17:00</div>
+              <div class="col-9 col-md-10">
+                Razglasitev zmagovalne ekipe, pica & pivo
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-faq" class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-lg-8">
+            <h2>FAQ</h2>
+            <div class="mb-4">
+              <h4>Kaj se na hekatonu pričakuje od mene?</h4>
+              <p>
+                Vabimo te, da z nami preživiš zabaven in izzivov poln dan
+                (09.00–17.00), raziščeš zbirke odprtih podatkov, s katerimi
+                upravlja Mestna občina Velenje, in ob pomoči mentorjev_ic
+                konceptualiziraš, skiciraš in razviješ prototipe rešitev, ki
+                bodo te podatke uporabile na zanimive načine.
+              </p>
+            </div>
+            <div class="mb-4">
+              <h4>Ali je programersko znanje obvezno?</h4>
+              <p>
+                Ne. Je pa zaželeno, da imaš izkušnje ali osnovna znanja s
+                področja spletnega oziroma digitalnega razvoja - torej
+                načrtovanje uporabniške izkušnje, oblikovanje, obdelava podatkov
+                ali kaj podobnega.
+              </p>
+            </div>
+            <div class="mb-4">
+              <h4>Zakaj bi to počel_a?</h4>
+              <p>
+                Ker te zanima razvoj družbeno koristnih orodij in ker imaš
+                priložnost, da si za en dan dela bogato nagrajen_a. Če
+                predstaviš izjemno uporaben koncept, so odprte tudi možnosti za
+                nadaljnje sodelovanje z Mestno občino Velenje in potencialna
+                realizacija tvoje rešitve!
+              </p>
+              <p>
+                Seveda bo poskrbljeno tudi za hrano, pijačo in prijetno druženje
+                – pa še naučil_a se boš kaj novega.
+              </p>
+            </div>
+            <div class="mb-4">
+              <h4>Potrebujem ekipo?</h4>
+              <p>
+                Hekalo se bo v ekipah, ki bodo imele od dva_e do štiri
+                člane_ice. Če imaš prijatelje_ice, ki so za akcijo, se prijavite
+                skupaj, sicer pa si lahko ekipo najdeš na dogodku.
+              </p>
+            </div>
+            <div class="mb-4">
+              <h4>Kako bo potekal izbor tekmovalk_cev?</h4>
+              <p>
+                Število mest je omejeno na 30. Izbiralo se bo udeležence_ke, ki
+                so visoko motivirani_e, navdihnjeni_e z dobrimi idejami in imajo
+                nekaj predhodnega tehnološkega znanja. Zato poskrbi, da se ob
+                prijavi dobro predstaviš.
+              </p>
+            </div>
+            <div class="mb-4">
+              <h4>Kateri koncepti bodo nagrajeni?</h4>
+              <p>
+                Zmagovalne koncepte bo izbrala štiričlanska žirija, sestavljena
+                iz predstavnic_kov Mestne občine Velenje, inštituta Danes je nov
+                dan in tehnološkega sponzorja.
+              </p>
+              <p>
+                Glavna kriterija za zmago sta ustvarjalnost in uporabnost ideje.
+              </p>
+            </div>
+            <div class="mb-4">
+              <h4>Kakšne so nagrade?</h4>
+              <p>
+                Zmagovalni koncept bo nagrajen s 700 evri drugo in tretje mesto
+                pa s 150 evri.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-challenges" class="container-fluid">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-lg-8">
+            <h2>IZZIVI</h2>
+            <p>
+              Pridi s povsem originalno idejo ali pa rešuj enega izmed
+              naslednjih izzivov:
+            </p>
+            <div>
+              <h4>Optimizacija kolesarskih poti</h4>
+              <!-- <p>
+                Vabimo te, da z nami preživiš zabaven in izzivov poln dan
+                (09.00–17.00), raziščeš zbirke odprtih podatkov, s katerimi
+                upravlja Mestna občina Velenje, in ob pomoči mentorjev_ic
+                konceptualiziraš, skiciraš in razviješ prototipe rešitev, ki
+                bodo te podatke uporabile na zanimive načine.
+              </p> -->
+            </div>
+            <div>
+              <h4>Semafor pomembnih projektov/odštevalnik</h4>
+            </div>
+            <div>
+              <h4>Interaktivni zemljevid zelenih površin</h4>
+            </div>
+            <div>
+              <h4>
+                Semafor pobud z možnostjo spremljanja napredka in sodelovanja
+              </h4>
+            </div>
+            <div>
+              <h4>
+                Chat bot na osnovi umetne inteligence (odgovori na vprašanja)
+              </h4>
+            </div>
+            <div>
+              <h4>AI učljivi Chat-Bot Vsevednik</h4>
+            </div>
+            <div>
+              <h4>Občinski Reddit</h4>
+            </div>
+            <div>
+              <h4>Nadgradnja portala za pobude servis48</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section-sponzors" class="container-fluid">
+      <div class="row justify-content-center">
+        <div
+          class="col-12 col-md-6 col-xl-5 d-flex justify-content-center justify-content-md-start align-items-center mb-3 mb-md-0"
+        >
+          <h5 class="text-center">Organizatorji</h5>
+          <a
+            href="https://danesjenovdan.si/"
+            target="_blank"
+            title="Danes je nov dan"
+            class="no-link-design"
+          >
+            <img src="/icons/hackathons/djnd.png" />
+          </a>
+          <a
+            href="https://sasainkubator.si/"
+            target="_blank"
+            title="SAŠA inkubator"
+            class="no-link-design"
+          >
+            <img src="/icons/hackathons/sasa_inkubator.png" />
+          </a>
+        </div>
+        <div
+          class="col-12 col-md-6 col-xl-5 d-flex justify-content-center justify-content-md-start align-items-center mb-3 mb-md-0"
+        >
+          <p class="text-center text-md-left">
+            Projekt sofinancira Ministrstvo za javno upravo iz Sklada za NVO.
+          </p>
+        </div>
+        <!-- <div
+          class="col-12 col-md-2 d-flex justify-content-center justify-content-md-end align-items-center"
+        >
+          <a class="facebook-event" href="https://xxx" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 16">
+              <g id="Layer_2" data-name="Layer 2">
+                <g id="Layer_1-2" data-name="Layer 1">
+                  <path
+                    class="cls-1"
+                    d="M6,6H9V9H6v7H3V9H0V6H3V4.75A5.65,5.65,0,0,1,4.12,1.23,3.59,3.59,0,0,1,6.9,0H9V3H6.9a.9.9,0,0,0-.9.9Z"
+                  />
+                </g>
+              </g>
+            </svg>
+          </a>
+        </div> -->
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  pageColor: 'primary',
+  components: {},
+  layout: 'checkout',
+  nuxtI18n: {
+    paths: {
+      sl: '/hack-velenje-2023',
+      en: '/hack-velenje-2023',
+    },
+  },
+  data() {
+    return {};
+  },
+  head() {
+    return {
+      title: 'Hack Velenje',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            '22. septembra pridi na enodnevni hekaton v SAŠA inkubator, spoznaj odprte zbirke podatkov Mestne občine Velenje in razvij koncept, ki ga boš uporabil_a na zanimiv in občanom_kam prijazen način!',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Hack Velenje',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            '22. septembra pridi na enodnevni hekaton v SAŠA inkubator, spoznaj odprte zbirke podatkov Mestne občine Velenje in razvij koncept, ki ga boš uporabil_a na zanimiv in občanom_kam prijazen način!',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://danesjenovdan.si/og-hack-velenje.jpg',
+        },
+        {
+          hid: 'twitter:title',
+          property: 'twitter:title',
+          content: 'Hack Velenje',
+        },
+        {
+          hid: 'twitter:description',
+          property: 'twitter:description',
+          content:
+            '22. septembra pridi na enodnevni hekaton v Tehnopark, spoznaj odprte zbirke podatkov Mestne občine Velenje in razvij koncept, ki ga boš uporabil_a na zanimiv in občanom_kam prijazen način!',
+        },
+        {
+          hid: 'twitter:image',
+          property: 'twitter:image',
+          content: 'https://danesjenovdan.si/og-hack-velenje.jpg',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+main {
+  font-family: 'aileron', sans-serif;
+}
+
+a:not(.no-link-design) {
+  font-size: 24px;
+  font-weight: bold;
+  color: #22b966;
+  background-color: #e8edfc;
+  border: 4px solid #22b966;
+  border-radius: 16px;
+  padding: 6px 24px;
+  transition: all 0.25s;
+}
+
+a:not(.no-link-design):hover {
+  text-decoration: none;
+  color: #e8edfc;
+  background-color: #22b966;
+}
+
+h2 {
+  font-size: 48px;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 20px;
+}
+
+.facebook-event:not(.no-link-design) {
+  // position: absolute;
+  // top: 30px;
+  // right: 0;
+  margin-right: 20px;
+  background-color: transparent;
+  border: 3px solid #e8edfc;
+  border-radius: 16px;
+  height: 48px;
+  width: 48px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    height: 28px;
+    width: 28px;
+  }
+
+  .cls-1 {
+    fill: #e8edfc;
+  }
+}
+
+.facebook-event:not(.no-link-design):hover {
+  background-color: #e8edfc;
+
+  .cls-1 {
+    fill: #000000;
+  }
+}
+
+@include media-breakpoint-up(md) {
+  a {
+    font-size: 36px;
+  }
+
+  h2 {
+    font-size: 72px;
+  }
+}
+
+#section-hero {
+  background-color: #000000;
+  padding: 40px 0;
+
+  h1 {
+    font-size: 56px;
+    font-weight: bold;
+    color: #428dff;
+    line-height: 0.75;
+    margin-bottom: 30px;
+    margin-top: 30px;
+  }
+
+  h2 {
+    font-size: 32px;
+    color: #22b966;
+    margin: 20px 0;
+  }
+
+  h3 {
+    font-size: 24px;
+    font-weight: bold;
+    color: #e8edfc;
+  }
+
+  p {
+    font-size: 20px;
+    color: #e8edfc;
+    margin-top: 30px;
+    line-height: 1.25;
+  }
+
+  @include media-breakpoint-up(md) {
+    h1 {
+      font-size: 100px;
+      font-weight: bold;
+      color: #b5c7fb;
+      margin-bottom: 30px;
+      -webkit-text-stroke: 5px;
+      -webkit-text-stroke-color: #428dff;
+    }
+
+    h2 {
+      font-size: 40px;
+      margin: 10px 0;
+    }
+
+    h3 {
+      font-size: 32px;
+    }
+
+    p {
+      font-size: 24px;
+      margin-top: 30px;
+    }
+  }
+
+  @include media-breakpoint-up(lg) {
+    h1 {
+      font-size: 120px;
+      font-weight: bold;
+      color: #b5c7fb;
+      -webkit-text-stroke: 5px;
+      -webkit-text-stroke-color: #428dff;
+    }
+
+    h2 {
+      font-size: 40px;
+      margin: 10px 0;
+    }
+
+    h3 {
+      font-size: 32px;
+    }
+
+    p {
+      font-size: 24px;
+      margin-top: 30px;
+    }
+  }
+}
+
+#section-graphics {
+  background-color: #000000;
+  padding-bottom: 30px;
+
+  .graphics-divider {
+    width: 40px;
+    height: 25px;
+    border-radius: 3px;
+    border: 4px solid #22b966;
+    background-color: #e8edfc;
+  }
+
+  img {
+    max-width: 80px;
+  }
+
+  @include media-breakpoint-up(lg) {
+    .graphics-divider {
+      width: 60px;
+      height: 30px;
+    }
+
+    img {
+      max-width: 120px;
+    }
+  }
+}
+
+#section-invite {
+  padding: 60px 0;
+
+  p {
+    font-size: 20px;
+    color: #000000;
+  }
+
+  p.registration-open {
+    font-size: 16px;
+  }
+
+  @include media-breakpoint-up(md) {
+    p {
+      font-size: 24px;
+    }
+  }
+}
+
+#section-awards {
+  background-color: #22b966;
+  color: #e8edfc;
+  padding: 60px 0;
+
+  .first-prize {
+    h2 {
+      font-size: 80px;
+      line-height: 0.75;
+    }
+  }
+
+  .second-prize {
+    h2 {
+      font-size: 54px;
+      line-height: 0.75;
+    }
+  }
+}
+
+#section-faq {
+  background-color: #e8edfc;
+  padding: 60px 0;
+  color: #000000;
+
+  h2 {
+    color: #22b966;
+  }
+
+  h4 {
+    font-size: 24px;
+    font-weight: bold;
+  }
+}
+
+#section-agenda {
+  background-color: #000000;
+  color: #e8edfc;
+  padding: 60px 0;
+  font-size: 20px;
+
+  @include media-breakpoint-up(md) {
+    font-size: 24px;
+  }
+
+  h2 {
+    color: #428dff;
+  }
+}
+
+#section-challenges {
+  background-color: #22b966;
+  color: #e8edfc;
+  padding: 60px 0;
+
+  h4 {
+    font-size: 24px;
+    font-weight: bold;
+  }
+}
+
+#section-sponzors {
+  background-color: #000000;
+  color: #e8edfc;
+  padding: 30px 0;
+
+  h5 {
+    margin: 0 20px;
+  }
+
+  p {
+    font-size: 16px;
+    margin-bottom: 0;
+  }
+
+  img {
+    margin-right: 20px;
+    max-width: 140px;
+  }
+}
+</style>
