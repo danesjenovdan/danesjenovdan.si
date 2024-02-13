4da8522a6de370d47e3f85cb3420708d83298651
Patricija Brečko <patricija@danesjenovdan.si>
2024-02-13 09:51:50 +0100

    move files to old and start new project

---
+++ djnd/djnd/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-y0p=ujxevu6)stsx!c(umnpz45#8@e)-xm-d@a(#22&jap(7!%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ djnd/djnd/urls.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.urls import include, path
+from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+from search import views as search_views
+
+urlpatterns = [
+    path("django-admin/", admin.site.urls),
+    path("admin/", include(wagtailadmin_urls)),
+    path("documents/", include(wagtaildocs_urls)),
+    path("search/", search_views.search, name="search"),
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Translatable URLs
+# These will be available under a language code prefix. For example /en/search/
+urlpatterns += i18n_patterns(
+    path("search/", search_views.search, name="search"),
+    path("", include(wagtail_urls)),
+    prefix_default_language=False,
+)
