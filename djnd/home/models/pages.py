a7bc6bdb0d88fb8e051032011d0c664963cb223d
Patricija Brečko <patricija@danesjenovdan.si>
2024-02-14 18:52:26 +0100

    add modular page and newsletter page

--- djnd/home/models/pages.py
+++ djnd/home/models/pages.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from wagtail.fields import StreamField
+from wagtail.fields import StreamField, RichTextField
 from wagtail.models import Page
 from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
@@ -57,13 +57,81 @@ class PillarPage(Page):
         FieldPanel("modules"),
     ]
 
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        # get activities for this pillar
+        # context["activities"] = 
+
+        return context
+
 
 class ModularPage(Page):
-    pass
+    subtitle = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    modules = StreamField(
+        ModuleBlock(), verbose_name="Moduli", null=True, blank=True, use_json_field=True
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+        FieldPanel("description"),
+        FieldPanel("image"),
+        FieldPanel("modules"),
+    ]
 
 
 class NewsletterPage(Page):
-    pass
+    introduction = RichTextField(blank=True, null=True)
+    news = StreamField(
+        [
+            (
+                "article",
+                blocks.StructBlock(
+                    [
+                        ("name", blocks.CharBlock()),
+                        ("description", blocks.CharBlock()),
+                        ("image", ImageChooserBlock()),
+                        ("link", blocks.URLBlock(required=False)),
+                    ]
+                ),
+            )
+        ],
+        null=True,
+        blank=True,
+        use_json_field=True,
+    )
+    exposed = StreamField(
+        [
+            (
+                "article",
+                blocks.StructBlock(
+                    [
+                        ("name", blocks.CharBlock()),
+                        ("description", blocks.CharBlock()),
+                        ("image", ImageChooserBlock()),
+                        ("link", blocks.URLBlock(required=False)),
+                    ]
+                ),
+            )
+        ],
+        null=True,
+        blank=True,
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("introduction"),
+        FieldPanel("news"),
+        FieldPanel("exposed"),
+    ]
 
 
 class BlogPage(Page):
