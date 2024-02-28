b5fb54ab72c3849bae9301cc3f27125778d22714
LepkoQQ <lepko.san@gmail.com>
2024-02-28 13:33:07 +0100

    add css compile

---
+++ css-compile/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./djnd/**/templates/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
+/*
+ - dark-green  #6CA89F
+ - dark-red    #DF786B
+ - dark-yellow #CFAD54
+
+light colors:
+ - mint     #9DF2D3
+ - red      #FFD1C7
+ - green    #D2F29D
+ - blue     #BAE2ED
+ - yellow   #FFEDAD
+ - lavender #CCC7FF
+*/
