e3a8327bf835ef78bf5ee425b27bcbf3182251d2
Patricija Brečko <patricija@danesjenovdan.si>
2024-05-23 12:20:12 +0200

    add newsletter functionality

--- djnd/djnd/static/js/script.js
+++ djnd/djnd/static/js/script.js
@@ -1,3 +1,47 @@
+document.addEventListener('alpine:init', () => {
+  Alpine.data('subscribe', () => ({
+    loading: false,
+
+    subscribeToNewsletter(id) {
+      this.loading = true;
+
+      const email = document.getElementById(`subscribe-email-${id}`).value;
+      console.log('email', email);
+      console.log('id', id);
+
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          segment_id: 21,
+        }),
+      };
+
+      fetch('https://podpri.lb.djnd.si/api/subscribe/', options)
+        .then((response) => response.json())
+        .then((data) => {
+          this.loading = false;
+          if (data.msg === 'mail sent') {
+            alert(
+              '{% translate "Hvala! Poslali smo ti sporočilo s povezavo, na kateri lahko potrdiš prijavo!" %}'
+            );
+            document.getElementById(`subscribe-email-${id}`).value = '';
+            document.getElementById(`subscribe-consent-${id}`).checked = false;
+          } else {
+            alert('{% translate "Prišlo je do napake :(" %}');
+          }
+        })
+        .catch((error) => {
+          this.loading = false;
+          console.error(error);
+        });
+    },
+  }));
+});
+
 function filterOurWork() {
   document.getElementById('our-work-form').submit();
 }
