f8df9f649cdc4ea9911a21f312e95da30a39f707
LepkoQQ <lepko.san@gmail.com>
2024-06-15 12:58:37 +0200

    add favicon

--- djnd/djnd/urls.py
+++ djnd/djnd/urls.py
@@ -1,19 +1,19 @@
 from django.conf import settings
-from django.urls import include, path
-from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
-
-from wagtail.admin import urls as wagtailadmin_urls
+from django.contrib import admin
+from django.templatetags.static import static
+from django.urls import include, path
+from django.views.generic.base import RedirectView
 from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=static("favicons/favicon.ico"))),
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
 ]
-
 
 if settings.DEBUG:
     from django.conf.urls.static import static
