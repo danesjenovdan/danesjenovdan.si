a0d24d9dcbfac6ccb0fc317013864fe74dbed648
LepkoQQ <lepko.san@gmail.com>
2024-06-20 12:44:16 +0200

    add filters to activity snippets

--- djnd/home/wagtail_hooks.py
+++ djnd/home/wagtail_hooks.py
@@ -1,17 +1,25 @@
 from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
 
 from home.models import (
+    Activity,
     ActivityCategory,
     ActivityProject,
-    Activity,
-    TeamMemberCategory,
-    TeamMember,
     Promoted,
+    TeamMember,
+    TeamMemberCategory,
 )
 
+
+class ActivityViewSet(SnippetViewSet):
+    model = Activity
+    list_display = ["title", "date", "promoted"]
+    list_filter = ["promoted", "pillar_page", "category", "project"]
+
+
+register_snippet(ActivityViewSet)
 register_snippet(ActivityCategory)
 register_snippet(ActivityProject)
-register_snippet(Activity)
-register_snippet(TeamMemberCategory)
-register_snippet(TeamMember)
 register_snippet(Promoted)
+register_snippet(TeamMember)
+register_snippet(TeamMemberCategory)
